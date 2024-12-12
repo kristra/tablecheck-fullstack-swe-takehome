f5867b08416e253c738d577f9ac02eceb04ec0a1
Krisnata <krisnata.panji@gmail.com>
2024-12-12 11:45:04 +0700

    feat: add sqlite and initial migrations

---
+++ prisma/migrations/20241212044304_create_table_queue/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Queue" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "sessionId" TEXT NOT NULL,
+    "checkInAt" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
