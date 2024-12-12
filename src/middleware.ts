5067f478010894bbf46f2a89d0d5cb74aabf5e2f
Krisnata <krisnata.panji@gmail.com>
2024-12-13 06:00:24 +0700

    refactor: handle rest api route separately in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,32 @@ import { cookies } from "next/headers";
 import { decrypt } from "./app/api/auth/session/session.service";
 
 const protectedRoutes = ["/reservations"];
-const publicRoutes = ["/", "/auth"];
+const protectedApiRoutes = ["/api/reservations"];
+const publicRoutes = ["/", "/api/auth"];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
+  const isApi = path.includes("/api/");
+  const isProtectedApiRoute = protectedApiRoutes.includes(path);
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
 
   const cookie = (await cookies()).get("session")?.value;
   const session = await decrypt(cookie);
 
-  if (isProtectedRoute && !session?.sessionId) {
-    return NextResponse.redirect(new URL("/", req.nextUrl));
-  }
+  if (isApi) {
+    // REST api handler
+    if (isProtectedApiRoute && !session?.sessionId) {
+      return NextResponse.json({ message: "unauthorized" }, { status: 403 });
+    }
+  } else {
+    if (isProtectedRoute && !session?.sessionId) {
+      return NextResponse.redirect(new URL("/", req.nextUrl));
+    }
 
-  if (isPublicRoute && session?.sessionId) {
-    return NextResponse.redirect(new URL("/reservations", req.nextUrl));
+    if (isPublicRoute && session?.sessionId) {
+      return NextResponse.redirect(new URL("/reservations", req.nextUrl));
+    }
   }
 
   return NextResponse.next();
@@ -26,5 +36,5 @@ export default async function middleware(req: NextRequest) {
 
 // Routes Middleware should not run on
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!_next/static|_next/image|.*\\.png$).*)"],
 };
